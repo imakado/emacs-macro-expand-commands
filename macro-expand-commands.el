6f52942ec207ca0986967ab675eac01c41fdef50
imakado <imakado@imakado-2.local>
2010-04-25 11:07:00 +0900

    initial commit.

---
+++ macro-expand-commands.el
@@ -0,0 +1,155 @@
+;;; macro-expand-commands.el - usefull commands for writting macro
+
+;; Copyright (C) 2010   IMAKADO
+
+;; Author: IMAKADO <ken.imakado@gmail.com>
+;; Keywords: emacs-lisp
+;; Prefix: macro-expand-commands-
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Note
+;; this package maight load "cl.el" (when called `macro-expand-commands-cl-macroexpand')
+
+;;; Commentary:
+
+;; Call comannd at the beggining of sexp.
+
+;; sample config
+;; (require 'macro-expand-commands)
+;; (define-key emacs-lisp-mode-map (kbd "C-M-S-e") 'macro-expand-cl-sexp)
+;; (define-key emacs-lisp-mode-map (kbd "M-E") 'macro-expand-sexp)
+;; (define-key emacs-lisp-mode-map (kbd "C-M-S-b") 'macro-expand-byte-code)
+
+(eval-when-compile
+  (require 'cl))
+(require 'rx)
+(require 'pp)
+
+(defvar macro-expand-commands-output-buffer-name "*MacroExpand output*")
+(defvar macro-expand-commands-output-function
+  (lambda (output-buffer)
+    (pop-to-buffer output-buffer)))
+
+(defvar macro-expand-commands-dont-newline-symbols
+  '( let let* quote while fset list lambda memq member member* car cdr
+         or prog1 not expect mapcar catch))
+
+(defun macro-expand-commands-cl-macroexpand (form &optional full)
+  (unless (fboundp 'cl-macroexpand-all)
+    (require 'cl))
+  (let ((cl-macroexpand-cmacs full)
+        (cl-compiling-file full)
+        (byte-compile-macro-environment nil))
+    (let ((ret-form (and (fboundp 'cl-macroexpand-all)
+                         (cl-macroexpand-all
+                          form
+                          (and (not full) '((block) (eval-when)))))))
+      (insert (format "%S" ret-form)))))
+
+(defmacro macro-expand-commands-cl-prettyexpand-string (macro)
+  (declare (debug (symbolp)))
+  `(progn
+     (with-temp-buffer
+       (macro-expand-commands-cl-macroexpand (quote ,macro))
+       (buffer-string))))
+
+(defmacro macro-expand-commands-cl-prettyexpand-all-string (macro)
+  `(progn
+     (with-temp-buffer
+       (macro-expand-commands-cl-macroexpand (quote ,macro) t)
+       (buffer-string))))
+
+(defmacro macro-expand-commands-macroexpand-string (macro)
+  `(progn
+     (format "%S" (macroexpand (quote ,macro)))))
+
+(defmacro macro-expand-commands-macroexpand-all-string (macro)
+  `(progn
+     (format "%S" (macroexpand-all (quote ,macro)))))
+
+(defmacro macro-expand-commands-compiled-byte-code-string (form)
+  `(progn
+     (format "%S" (byte-compile (lambda () ,form)))))
+
+(defun* macro-expand-commands-aux (pp-macro-name &key (pp t))
+  (let ((sexp (thing-at-point 'sexp)))
+    (let ((ret (eval (first (read-from-string
+                             (format "(%s  %s)" pp-macro-name sexp))))))
+      (let ((outbuf (get-buffer-create macro-expand-commands-output-buffer-name)))
+        (with-current-buffer outbuf
+          (erase-buffer)
+          (emacs-lisp-mode)
+          (insert (format "%s" ret))
+          ;; remove newline at bob and eob
+          ;; replace newline to \\n (this makes fontify and indentation broken)
+          ;; beautify buffer
+          (when pp
+            (macro-expand-commands-pp-output-buffer (current-buffer)))
+          (funcall macro-expand-commands-output-function outbuf))))))
+
+(defun macro-expand-commands-pp-output-buffer (buf)
+  (with-current-buffer buf
+    (flet ((trim (re &optional (newstring "") (pred 'identity))
+                 (save-excursion
+                   (loop initially (goto-char (point-min))
+                         while (re-search-forward re nil t)
+                         when (funcall pred (point))
+                         do (typecase newstring
+                              (string (replace-match newstring))
+                              (function (funcall newstring)))))))
+      (trim (rx buffer-start (+ (any space "\n"))))
+      (trim (rx (+ (any space "\n")) buffer-end))
+      (trim "\n" "\\\\n" (lambda (ignore)
+                           (let ((in-string?
+                                  (nth 3 (save-excursion
+                                           (parse-partial-sexp (point-min) (point))))))
+                             in-string?)))
+      (pp-buffer)
+      (loop for sym in macro-expand-commands-dont-newline-symbols
+            for sym-str = (symbol-name sym)
+            for re = (rx-to-string
+                      `(seq symbol-start ,sym-str symbol-end "\n" (* space)))
+            do (trim re (concat sym-str " ")))
+      (indent-region (point-min) (point-max)))))
+
+(defun macro-expand-sexp (&optional prefix)
+  (interactive "P")
+  (if prefix
+      (macro-expand-commands-aux "macro-expand-commands-macroexpand-all-string")
+    (macro-expand-commands-aux "macro-expand-commands-macroexpand-string")))
+
+(defun macro-expand-all-sexp ()
+  (interactive)
+  (macro-expand-commands-aux "macro-expand-commands-macroexpand-all-string"))
+
+(defun macro-expand-cl-sexp (&optional prefix)
+  (interactive "P")
+  (if prefix
+      (macro-expand-commands-aux "macro-expand-commands-cl-prettyexpand-all-string")
+    (macro-expand-commands-aux "macro-expand-commands-cl-prettyexpand-string")))
+
+(defun macro-expand-cl-all-sexp ()
+  (interactive)
+  (macro-expand-commands-aux "macro-expand-commands-cl-prettyexpand-all-string"))
+
+(defun macro-expand-byte-code (&optional prefix)
+  (interactive "P")
+  (macro-expand-commands-aux "macro-expand-commands-compiled-byte-code-string"
+                             :pp nil))
+
+(provide 'macro-expand-commands)
+;; macro-expand-commands.el ends here.
